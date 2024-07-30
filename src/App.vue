d078df68950a1bdda332a184574bf7e1badae57f
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-30 17:53:26 +0200

    [upd] Milestone 4 - results correctly recived in console after axios call

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
-<script setup>
+<script>
+import axios from 'axios';
+import { store } from './store'
+
+export default {
+  name: 'vite-boolfolio',
+
+
+  data() {
+    return {
+      store
+    }
+  },
+
+  created() {
+    axios.get(this.store.apiUrl).then(response => {
+      console.log('Projects:', response.data);
+    });
+  }
+}
 
 </script>
 
 <template>
-  ciao
+  ciao mondo
 </template>
 
 <style scoped lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import './components/scss/style.scss'
-
-
-import * as bootstrap from "bootstrap";
-
+import * as bootstrap from "bootstrap"
 import App from './App.vue'
 
 createApp(App).mount('#app')
