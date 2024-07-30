989aad1ae61ea14d6224acc1c6df6f39395a0f89
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-30 18:38:09 +0200

    [upd] Milestone 5 - card with info

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,8 @@ export default {
 
   created() {
     axios.get(this.store.apiUrl).then(response => {
-      console.log('Projects:', response.data);
+      this.store.results = response.data.results;
+      console.log('Projects:', this.store.results);
     });
   }
 }
@@ -22,7 +23,23 @@ export default {
 </script>
 
 <template>
-  ciao mondo
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h1 class="mb-5">BoolFolio</h1>
+        <div class="d-flex justify-content-center flex-wrap">
+          <div class="card mx-3" v-for="(result, index) in store.results">
+            <img class="card-img-top" :src='result.cover' alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">{{ result.title }}</h5>
+              <p class="card-text">{{ result.description }}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
+    results: [],
     apiUrl: 'http://127.0.0.1:8000/api/projects'
 })
+
+
+
